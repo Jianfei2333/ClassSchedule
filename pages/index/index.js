6ad39e74d19542c209875e209df72dfa49ec8a1a
Jianfei2333 <xjf999999@hotmail.com>
2017-10-14 22:28:27 +0800

    add manual package, add spider program (parser ready, but can't be called, prepare to fix)

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,6 @@
 const app = getApp()
 const util = require('../../utils/util')
+const moment = require('../../package/moment/moment')
 
 Page({
   data: {
@@ -7,7 +8,15 @@ Page({
     Student_info: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    today: '10.11'
+    date: [moment().startOf('week').format('MM.DD'),
+      moment().startOf('week').add(1, 'd').format('MM.DD'),
+      moment().startOf('week').add(2, 'd').format('MM.DD'),
+      moment().startOf('week').add(3, 'd').format('MM.DD'),
+      moment().startOf('week').add(4, 'd').format('MM.DD'),
+      moment().startOf('week').add(5, 'd').format('MM.DD'),
+      moment().startOf('week').add(6, 'd').format('MM.DD'),],
+    semester_picker:[['2015', '2016', '2017', '2018', '2019'], ['春季', '秋季']],
+    year_semester: [0, 0],
   },
 
   bindViewTap: function() {
@@ -18,8 +27,10 @@ Page({
 
   onLoad: function () {
     let info = app.globalData.Student_info
+    let ys = wx.getStorageSync('year_semester')
     this.setData({
-      Student_info: info
+      Student_info: info,
+      year_semester: ys
     })
     let v = app.globalData.Union_id
     if (!v) {
@@ -45,6 +56,15 @@ Page({
         url: '../login/login',
       })
     }
+  },
+
+  change_selector: function(e) {
+    console.log(e.detail.value)
+    let p = this
+    this.setData({
+      year_semester: e.detail.value
+    })
+    wx.setStorageSync('year_semester', e.detail.value)
   },
 
 })
